e2e53ab621e20b643343d4742a8d2d3f41ba8ba9
lukaszmapper <lukideja@gmail.com>
2023-12-01 18:36:49 +0100

    Widoki

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const connection = mysql.createConnection({
+    host: 'roundhouse.proxy.rlwy.net',
+    user: 'root',
+    password: '',
+    database: 'railway',
+    port: 45362
+});
+
 app.listen(port, () => {
 
 })
