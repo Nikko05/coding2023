3bb56611d21922f27c708c1f4ebd4089f0010e51
Seb-M11 <sebastian.12345.ns.67@gmail.com>
2023-12-01 18:46:31 +0100

    elo

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ const app = express()
 const port = 3000
 const mysql = require('mysql')
 const bodyParser = require("body-parser")
-const bcrypt = require("bcrypt")
-const fetch = require("node-fetch")
 const cookieParser = require("cookie-parser")
-const crypto = require("crypto")
-const { text } = require('body-parser')
-const axios = require("axios")
+
+
 
 app.set('view-engine', 'ejs')
 app.use(cookieParser())
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('Hello World!sdsds')
 })
 
 const connection = mysql.createConnection({
