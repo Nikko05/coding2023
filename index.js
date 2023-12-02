575ac8ad2bc9a11935da983e0c9e28b21bb0ed78
K0S4 <szymonkowalski142@gmail.com>
2023-12-02 02:47:46 +0100

    created profile page

--- index.js
+++ index.js
@@ -1,138 +1,161 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const mysql = require('mysql2')
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
-const bcrypt = require("bcrypt")
+const express = require("express");
+const app = express();
+const port = 3000;
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
 
-
-app.set('view-engine', 'ejs')
-app.use(cookieParser())
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.set("view-engine", "ejs");
+app.use(cookieParser());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const connection = mysql.createConnection({
-    host: 'roundhouse.proxy.rlwy.net',
-    user: 'root',
-    password: "-1GHde2Ad5C3gfBA-Ga22ddB1gBfhE5e",
-    database: 'railway',
-    port: 42881
+  host: "roundhouse.proxy.rlwy.net",
+  user: "root",
+  password: "-1GHde2Ad5C3gfBA-Ga22ddB1gBfhE5e",
+  database: "railway",
+  port: 42881,
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-const path = require('path')
-app.use(express.static(path.join(__dirname, 'public')));
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/register', (req, res) => {
-  res.render('register.ejs')
-})
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
 
-app.get('/login', (req, res) => {
-    if (req.cookies['user']) {
-        res.redirect('/')
-    } else {
-        res.render('login.ejs')
-    }
-})
+app.get("/login", (req, res) => {
+  if (req.cookies["user"]) {
+    res.redirect("/");
+  } else {
+    res.render("login.ejs");
+  }
+});
 
-app.post('/register', urlencodedParser, (req,res) => {
-    var imie = req.body.imie
-    var nazwisko = req.body.nazwisko
-    var email = req.body.email
-    var login = req.body.login
-    var pass1 = req.body.pass1
-    var pass2 = req.body.pass2
-    var pass
-    var admin = 0
-    connection.query(`SELECT id FROM users WHERE login = '${login}';`, function (err, result, fields) {
-    if (Object.keys(result).length > 0){
-        res.send('Juz istnieje taki uzytkownik')
-    } else {    
+app.post("/register", urlencodedParser, (req, res) => {
+  var imie = req.body.imie;
+  var nazwisko = req.body.nazwisko;
+  var email = req.body.email;
+  var login = req.body.login;
+  var pass1 = req.body.pass1;
+  var pass2 = req.body.pass2;
+  var pass;
+  var admin = 0;
+  connection.query(
+    `SELECT id FROM users WHERE login = '${login}';`,
+    function (err, result, fields) {
+      if (Object.keys(result).length > 0) {
+        res.send("Juz istnieje taki uzytkownik");
+      } else {
         if (pass1 == pass2) {
-            bcrypt.hash(pass1, 10, function(err, hash) {
-                connection.query(`INSERT INTO users (imie, nazwisko, email, login, haslo, admin) VALUES ('${imie}', '${nazwisko}', '${email}', '${login}', '${hash}', '${admin}');`, function (err, result) {
-                    if (err) throw err
-                    res.redirect('/login')
-                })
-            });
+          bcrypt.hash(pass1, 10, function (err, hash) {
+            connection.query(
+              `INSERT INTO users (imie, nazwisko, email, login, haslo, admin) VALUES ('${imie}', '${nazwisko}', '${email}', '${login}', '${hash}', '${admin}');`,
+              function (err, result) {
+                if (err) throw err;
+                res.redirect("/login");
+              }
+            );
+          });
         } else {
-            res.send("Hasła się różnią")
+          res.send("Hasła się różnią");
         }
+      }
     }
-    })
-})
+  );
+});
 
-app.get('/', (req, res) => {
-    var cookie
-    if (req.cookies['user']) {
-        cookie = req.cookies['user']
-        var wyswietl = "<html><head><title>Zagrozenia</title></head><body>"
-        connection.query(`SELECT * FROM dangers ORDER BY data DESC;`, function(err, result, fields) {
-            if (Object.keys(result).length > 0){
-                for (var i = 0; i < Object.keys(result).length; i++) {
-                    wyswietl += "<div>"
-                    wyswietl += "<p>" + result[i].lokacja + "</p>"
-                    wyswietl += "<p>" + result[i].data + "</p>"
-                    wyswietl += "<p>" + result[i].rodzaj + "</p>"
-                    wyswietl += "<p>" + result[i].opis + "</p></div>"
-                }
+app.get("/", (req, res) => {
+  var cookie;
+  if (req.cookies["user"]) {
+    cookie = req.cookies["user"];
+    var wyswietl = "<html><head><title>Zagrozenia</title></head><body>";
+    connection.query(
+      `SELECT * FROM dangers ORDER BY data DESC;`,
+      function (err, result, fields) {
+        if (Object.keys(result).length > 0) {
+          for (var i = 0; i < Object.keys(result).length; i++) {
+            wyswietl += "<div>";
+            wyswietl += "<p>" + result[i].lokacja + "</p>";
+            wyswietl += "<p>" + result[i].data + "</p>";
+            wyswietl += "<p>" + result[i].rodzaj + "</p>";
+            wyswietl += "<p>" + result[i].opis + "</p></div>";
+          }
+        }
+        wyswietl += "</body></html>";
+        res.send(wyswietl);
+      }
+    );
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/login", urlencodedParser, (req, res) => {
+  var login = req.body.login;
+  var pass = req.body.pass;
+  connection.connect(function (err) {
+    connection.query(
+      `SELECT haslo FROM users WHERE login="${login}"`,
+      function (err, result, fields) {
+        if (Object.keys(result).length > 0) {
+          bcrypt.compare(pass, result[0].haslo, function (err, result) {
+            if (result) {
+              console.log(result);
+              res.cookie("user", login);
+              res.redirect("/");
+            } else {
+              res.send("Złe hasło");
             }
-            wyswietl += "</body></html>"
-            res.send(wyswietl)
-        })
-    } else {
-        res.redirect('/login')
-    }
-})
-
-app.post("/login", urlencodedParser, (req, res)=>{
-    var login = req.body.login
-    var pass = req.body.pass
-    connection.connect(function(err) {
-        connection.query(`SELECT haslo FROM users WHERE login="${login}"`, function (err, result, fields) {
-        if (Object.keys(result).length > 0){
-            bcrypt.compare(pass, result[0].haslo, function (err, result) {
-                if (result) {
-                    console.log(result)
-                    res.cookie("user", login)
-                    res.redirect("/")
-                } else {
-                    res.send("Złe hasło")
-                }
-            })
+          });
         } else {
-            res.send("Nie ma takiego uzytkownika")
+          res.send("Nie ma takiego uzytkownika");
         }
-        })
+      }
+    );
+  });
+});
+app.get("/logout", (req, res) => {
+  res.clearCookie("user", { domain: "localhost" });
+  res.redirect("/login");
+});
+
+app.get("/profile", urlencodedParser, (req, res) => {
+  let cookie = req.cookies["user"];
+
+  connection.query(
+    `SELECT imie, nazwisko, login, email FROM users WHERE login = '${cookie}'`,
+    function (err, result, fields) {
+      if (err) {
+        console.error("Error executing query:", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      // Check if a user was found
+      if (result.length > 0) {
+        let imie = result[0].imie;
+        let nazwisko = result[0].nazwisko;
+        let login = result[0].login;
+        let email = result[0].email;
+
+        res.render("profile.ejs", {
+          imie: imie,
+          nazwisko: nazwisko,
+          login: login,
+          email: email,
+        });
+      } else {
+        // No user found with the given cookie value
+        res.status(404).send("User not found");
+      }
     }
-        
-    
-   )
-  })
-  app.get('/logout', (req, res) => {
-    res.clearCookie('user', {domain: 'localhost'});
-    res.redirect('/login')
-})
+  );
+});
 
-app.get("/profile", urlencodedParser, (req, res)=>{
-  res.render("profile.ejs");
-})
-
-
-app.post("/profile", urlencodedParser, (req, res)=>{
-  let name = req.body.name;
-  let surname = req.body.surname;
-  let birthDate = req.body.birthDate;
-  let bio = req.body.bio;
-  let bloodType = req.body.bloodType;
-
-  res.redirect("/");
-})
-
-
-app.listen(port, () => {
-
-})
+app.listen(port, () => {});
